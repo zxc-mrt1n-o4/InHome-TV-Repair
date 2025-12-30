8d952c1d06f0b9878e70a963eb5ce1ff50009dbe
zxc-mrt1n-o4 <gamexoficall@gmail.com>
2025-12-30 17:40:36 -0500

    robots upd

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,11 +6,13 @@ import { MetadataRoute } from 'next';
  */
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
-    sitemap: 'https://inhometvrepair.com/sitemap.xml', // Update with your actual domain
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
+    sitemap: 'https://inhometvrepair.com/sitemap.xml',
   };
 }
